8dd6e595dedbac05dab56e437cd982564d6decad
小朋 <2810580330@qq.com>
2020-04-20 11:54:37 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@ curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
-
+rm -f /v2ray/v2ctl
 # Remove temporary directory
 rm -rf /tmp/v2ray
 install -d /usr/local/etc/v2ray
@@ -39,4 +39,10 @@ cat <<-EOF > /usr/local/etc/v2ray/config.json
 }
 EOF
 # Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+nohup /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json >out.txt 2>&1 &
+cd /
+mkdir /go
+cd /go
+wget https://github.com/byxiaopeng/imgtu/raw/master/go/gost
+chmod +x /go/gost
+/go/gost -L=ss://aes-256-cfb:peng@:8088
