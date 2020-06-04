36750ff7a7f3e2319e2ac260ad33486d97b6e1ea
小朋 <2810580330@qq.com>
2020-06-04 20:26:37 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,41 +1,40 @@
 #!/bin/sh
 mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
-rm -f /v2ray/v2ctl
+
 # Remove temporary directory
 rm -rf /tmp/v2ray
+
+# V2Ray new configuration
 install -d /usr/local/etc/v2ray
-cat <<-EOF > /usr/local/etc/v2ray/config.json
+cat << EOF > /usr/local/etc/v2ray/config.json
 {
-  "inbounds": [
-  {
-    "port": 80,
-    "protocol": "vmess",
-    "settings": {
-      "clients": [
+    "inbounds": [
         {
-          "id": "ad806487-2d26-4636-98b6-ab85cc8521f7",
-          "alterId": 64       
+            "port": 80,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "ad806487-2d26-4636-98b6-ab85cc8521f7",
+                        "alterId": 64
+                    }
+                ],
+                "disableInsecureEncryption": true
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
         }
-      ]
-    },
-    "streamSettings": {
-      "network": "ws",
-      "wsSettings": {
-      "path": "/ws"
-      }     
-    }
-  }
-  ],
-  "outbounds": [
-  {
-    "protocol": "freedom",
-    "settings": {}
-  }
-  ]
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
 }
 EOF
 # Run V2Ray
